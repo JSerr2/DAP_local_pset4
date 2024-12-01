84880eb34eb1d10fbff980fde835d73d18804e0e
JSerr2 <Joseserranolansing@gmail.com>
2024-11-30 23:40:41 -0600

    added commentary

--- program_commented.py
+++ program_commented.py
@@ -20,31 +20,39 @@ def myfunction1(mylist, mynumber2):
     print("Welcome!")
     print("You have {mynumber2} attempts.")
     
+# Starts a loop where the player will guess the word    
     while mynumber1 < mynumber2:
+# the player will put input their guess here
         mystring2 = input("Enter your guess: ").lower().strip()
-        
+# this will check if the word is a five letter word or not and print invalid input if its not        
         if len(mystring2) != 5 or not mystring2.isalpha():
             print("Invalid input.")
             continue
-        
+# Increases how many attempts the player has        
         mynumber1 = mynumber1 + 1
-        
+# This part checks if the player guessed the correct word        
         if mystring2 == mystring1:
             print(f"Congratulations! You won in {mynumber1} attempts.")
-            break
+            break # ends the loop if the player guesses correctly
         else:
+          
+          # this part of the code generates a hint by comparing the guessed word and the selected word
             mymessage = ''
             for i in range(5):
                 if mystring2[i] == mystring1[i]:
                     mymessage = mymessage + mystring1[i]
                 else:
                     mymessage = mymessage + '_'
+        # Here, the code calculates the number of remaining attempts
+                    
             mynumber3 = mynumber2 - mynumber1
             print(f"Wrong! Here's what you got right: {mymessage}")  
             print(f"You have {mynumber3} attempts left.")
     
+    # Whe player runs out of attempts, the correct word is revealed
+    
     if mynumber1 == mynumber2:
         print(f"Sorry, you lost. The correct answer was: '{mystring1}'.")
-
+# Calls the function and uses 5 attempts
 myfunction1(mylist, 5)
 
